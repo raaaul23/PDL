ee0e87b1be0867bbe48fe665d0361b67d4f84fd9
raaaul23 <raullopezkloda@gmail.com>
2024-10-11 21:29:31 +0200

    Cambiado la ruta del fichero
    Para que todos podamos utilizarlo sin problema

--- src/Principal/Main.java
+++ src/Principal/Main.java
@@ -1,18 +1,16 @@
 package Principal;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import Accion.LeerFichero;
 import Analizadores.Analizador_Lexico;
 
 public class Main {
 	public static void main(String [] args) {
-		String rutaArchivoEntrada = "C:\\Users\\danig\\eclipse-workspace\\PDL\\src\\prueba.txt";
+		String rutaArchivoEntrada = ".\\prueba.txt";
 		LeerFichero fich= new LeerFichero(rutaArchivoEntrada);
 		Analizador_Lexico an_lex=new Analizador_Lexico(fich);
 		try {
-			an_lex.escribirTokens("C:\\Users\\danig\\eclipse-workspace\\PDL\\src\\salida.txt");
+			an_lex.escribirTokens(".\\salida.txt");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
